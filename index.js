355ef8904f5946b3a4359b5c08aa43fc60a5f1da
Eren <erensmr63@gmail.com>
2024-09-05 22:59:55 +0300

    fixed fatal error of not working

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const SourceBit = require('./lib/application');
-const Router = require('./lib/core/router');
-const Request = require('./lib/core/request');
-const Response = require('./lib/core/response');
-const ViewSet = require('./lib/views/viewsets');
-module.exports = {
-  SourceBit,
-  Router,
-  Request,
-  Response,
-  ViewSet,
-};
+const SourceBit = require("./lib/application");
+
+module.exports = SourceBit;
